00d4b4b6bd74db07caf6049e0e4fa4dca3e62dcd
Harry Southworth <harry.southworth@gmail.com>
2013-05-10 00:00:00 +0000

    version 2.1

--- demo/robustReg.R
+++ demo/robustReg.R
@@ -12,17 +12,18 @@ d <- data.frame( y=y , x)
 
 gmod <- gbm( y ~ ., data=d, distribution="gaussian",
              n.tree = 2000, shrinkage = .01 , cv.folds=5,
-            verbose = FALSE)
+            verbose = FALSE, n.cores=1)
 tmod4 <- gbm( y ~ ., data=d, distribution="tdist", # defaults to 4 df
-              n.tree=2000, shrinkage = .01, cv.folds=5 )
+              n.tree=2000, shrinkage = .01, cv.folds=5,
+             verbose = FALSE, n.cores=1)
 tmod6 <- gbm( y ~ ., data=d, distribution=list( name="tdist", df=6 ),
               n.tree=2000, shrinkage = .01, cv.folds=5,
-             verbose = FALSE)
+              verbose = FALSE, n.cores=1)
 tmod100 <- gbm( y ~ ., data=d, distribution=list( name="tdist", df=100 ),
               n.tree=2000, shrinkage = .01, cv.folds=5,
-               verbose = FALSE)
+               verbose = FALSE, n.cores=1)
 
-par(mfrow=c( 2, 3 ) )
+par(mfrow=c( 2, 2 ) )
 gbest <- gbm.perf( gmod , method="cv" )
 t4best <- gbm.perf( tmod4 , method="cv" )
 t6best <- gbm.perf( tmod6 , method="cv" )
@@ -30,14 +31,14 @@ t100best <- gbm.perf( tmod100 , method="cv" )
 
 qscale <- function( x ){
   x / abs( diff( quantile( x , prob=c( .25, .75 ) ) ) )
-}  
+}
 
 rg <- qscale( resid( gmod , n.trees=gbest) )
 rt4 <- qscale( resid( tmod4 , n.trees=t4best) )
 rt6 <- qscale( resid( tmod6 , n.trees=t6best) )
 rt100 <- qscale( resid( tmod100 , n.trees=t100best ) )
 
-ylimits <- range( rg , rb8 , rb95 , rt4, rt6, rt100 )
+ylimits <- range(rg, rt4, rt6, rt100)
 
 plot( rg, main="Gaussian", ylim=ylimits ); abline( h=0 )
 plot( rt4, main="t(4)", ylim=ylimits ); abline( h=0 )
